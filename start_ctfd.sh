29ff1d60c47719913c58e55946c77d59fc58df53
Chiara Knicker <chiara.knicker@gmail.com>
2025-03-03 20:27:01 +0000

    ctfd server config

--- start_ctfd.sh
+++ start_ctfd.sh
@@ -19,11 +19,6 @@ if [ -z "$PROVIDER" ]; then
 fi
 
 # Check if required environment variables are set
-if [ -z "$CTF_YEAR" ]; then
-    echo "Error: CTF_YEAR is not defined in .env"
-    exit 1
-fi
-
 if [ -z "$SSH_USER" ]; then
     echo "Error: SSH_USER is not defined in .env"
     exit 1
@@ -34,12 +29,11 @@ if [ -z "$SSH_PRIVATE_KEY" ]; then
     exit 1
 fi
 
-echo "Starting CTF setup for year: ${CTF_YEAR}"
-
-cd "terraform/ctfd/$PROVIDER"
+echo "Starting CTFd setup..."
 
 # Initialize Terraform
 echo "Initializing Terraform..."
+cd "terraform/ctfd/$PROVIDER"
 terraform init
 
 # Apply Terraform configuration
@@ -52,6 +46,7 @@ CTFD_IP=$(terraform output -raw ctfd_instance_ip)
 cd ../../..
 
 # Update .env file
+echo "Adding VM IP to .env..."
 if grep -q "^CTFD_IP=" ".env"; then
     sed -i "s|^CTFD_IP=.*|CTFD_IP=$CTFD_IP|" ".env"
 else
@@ -61,13 +56,10 @@ echo "Updated .env file successfully!"
 
 # Try to SSH once VM is ready
 max_retries=5
-wait_time=10 #seconds
-
+wait_time=15 #seconds
 set +e
 for ((retry_count=1; retry_count<=max_retries; retry_count++)); do
-    echo -ne "Attempting to SSH... ($retry_count/$max_retries)                  \r"
-
-    # Attempt SSH
+    echo -ne "Attempting to SSH... ($retry_count/$max_retries)                           \r"
     ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no -o ConnectTimeout=5 $SSH_USER@$CTFD_IP exit 2>/dev/null
 
     # Check if SSH connection is successful
@@ -90,7 +82,7 @@ set -e
 # Wait for cloud-init to finish
 echo -e "\nWaiting for cloud-init to complete..."
 ssh -T -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP <<EOF
-    TOTAL_LINES=1615 
+    TOTAL_LINES=1678
 
     while ! grep -q 'Cloud-init.*finished' /var/log/cloud-init-output.log; do
         CURRENT_LINES=\$(wc -l < /var/log/cloud-init-output.log)
@@ -104,14 +96,42 @@ EOF
 echo -e "\nCloud-init finished!"
 
 # Add CTFd Theme
-echo "Adding CTFd themes..."
+echo "Adding CTFd theme..."
 ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP "sudo chown -R $SSH_USER:$SSH_USER /opt/CTFd/CTFd/themes/" # Give ubuntu user permissions to write to themes directory
 #scp -i "$SSH_PRIVATE_KEY" -r ctfd_theme/uclcybersoc $SSH_USER@$CTFD_IP:/opt/CTFd/CTFd/themes/
 #scp -i "$SSH_PRIVATE_KEY" -r ctfd_theme/ucl-core $SSH_USER@$CTFD_IP:/opt/CTFd/CTFd/themes/
-scp -i "$SSH_PRIVATE_KEY" -r ctfd_theme/porticoHack $SSH_USER@$CTFD_IP:/opt/CTFd/CTFd/themes/
+#scp -i "$SSH_PRIVATE_KEY" -r ctfd_theme/porticoHack $SSH_USER@$CTFD_IP:/opt/CTFd/CTFd/themes/
+
+# Update docker-compose.yml
+echo "Updating docker-compose.yml file..."
+ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP "sudo chown -R $SSH_USER:$SSH_USER /opt/CTFd/"
+scp -i "$SSH_PRIVATE_KEY" docker-compose.patch $SSH_USER@$CTFD_IP:/opt/CTFd/
+ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP "patch /opt/CTFd/docker-compose.yml < /opt/CTFd/docker-compose.patch"
+
+# Create a file at /etc/nginx/sites-available/DOMAIN and fill with nginx-rate-limit-proxy.conf
+# This sets up rate-limiting at 10 requests per second, and a max of 10 simultaneous connections per IP address at a time, 
+# and also tells Nginx to route requests to DOMAIN at port 8000.
+echo "Adding nginx-rate-limit-proxy to /etc/nginx/sites-available/$CTFD_SUBDOMAIN.$DOMAIN..."
+ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP "sudo chown -R $SSH_USER:$SSH_USER /etc/nginx/sites-available/"
+scp -i "$SSH_PRIVATE_KEY" server_config/nginx-rate-limit-proxy.conf $SSH_USER@$CTFD_IP:/etc/nginx/sites-available/$CTFD_SUBDOMAIN.$DOMAIN
+ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP "sed -i 's/DOMAIN/$CTFD_SUBDOMAIN.$DOMAIN/g' /etc/nginx/sites-available/$CTFD_SUBDOMAIN.$DOMAIN" # Update Nginx config file with actual domain
+
+# Create a symlink to the file you created in the previous step in /etc/nginx/sites-enabled and reload Nginx
+echo "Creating symlink..."
+ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP "sudo ln -sf /etc/nginx/sites-available/$CTFD_SUBDOMAIN.$DOMAIN /etc/nginx/sites-enabled/$CTFD_SUBDOMAIN.$DOMAIN"
+ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP "sudo nginx -t" # Test for syntax errors
+ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP "sudo systemctl restart nginx"
+
+# Edit http section in /etc/nginx/default to log the real User IP not cloudflare IPs
+#TODO
+
+# get SSL certificate
+#echo "Getting SSL certificate..."
+#ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP "sudo certbot --nginx -d $SUBDOMAIN.$DOMAIN --non-interactive --agree-tos --email $EMAIL --redirect"
+#ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP "sudo systemctl restart nginx"
 
 # SSH into the VM and deploy CTFd using Docker Compose
-echo "Deploying CTFd with Docker Compose..."
+echo "Deploying CTFd with docker compose..."
 ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP <<EOF
     cd /opt/CTFd
 
@@ -122,21 +142,4 @@ ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP <<EOF
     sudo docker-compose up -d
 EOF
 
-# get SSL certificate
-#echo "Getting SSL certificate..."
-#ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP <<EOF
-#    sudo certbot --nginx -d $CTFD_DOMAIN
-#EOF
-
-# nginx configuration
-#echo "Configuring nginx..."
-#ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP "sudo chown -R $SSH_USER:$SSH_USER /etc/nginx/sites-available/" # Give ubuntu user permissions to write to themes directory
-#scp -i "$SSH_PRIVATE_KEY" config/nginx.conf $SSH_USER@$CTFD_IP:/etc/nginx/sites-available/ctfd
-#ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP "sed -i 's/your-domain/$CTFD_DOMAIN/g' /etc/nginx/sites-available/ctfd" # Update Nginx config file with actual IP before copying
-#ssh -i "$SSH_PRIVATE_KEY" $SSH_USER@$CTFD_IP <<EOF
-#    sudo ln -sf /etc/nginx/sites-available/ctfd /etc/nginx/sites-enabled/
-#    sudo nginx -t  # Test for syntax errors
-#    sudo systemctl restart nginx
-#EOF
-
-echo "CTF setup complete for year ${CTF_YEAR}!"
+echo "CTFd setup complete!"
--- terraform/ctfd/oracle/scripts/ctfd-init.sh
+++ terraform/ctfd/oracle/scripts/ctfd-init.sh
@@ -5,15 +5,24 @@
 # Update and install dependencies
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt-get install -y git docker.io docker-compose python3-pip nginx certbot python3-certbot-nginx rsync
+sudo apt-get install -y git docker.io docker-compose python3-pip nginx certbot python3-certbot-nginx ufw rsync
 
 # Enable Docker to start on boot
 sudo systemctl enable docker
 sudo systemctl start docker
 
-# Enable Nginx and start it
-sudo systemctl enable nginx
-sudo systemctl start nginx
+# Allow ssh, HTTP, and HTTPS through the firewall
+sudo ufw allow 'Nginx Full'
+sudo ufw allow 'OpenSSH'
+
+# Enable the firewall
+sudo ufw enable
+
+# For setting up HTTPS
+sudo apt-get install software-properties-common
+sudo add-apt-repository universe
+sudo apt-get update
+sudo apt-get install certbot python3-certbot-nginx
 
 # Clone CTFd repository
 cd /opt
